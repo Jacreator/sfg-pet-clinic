1a8fa5e24bc79e105814448f80bca95b0b9c0fba
Jacreator <jambone.james82@gmail.com>
2020-08-04 16:40:27 +0100

    Revert "completed Interface services closes #5"
    This reverts commit 1009dce44099ec0d08a3f128f7ff10495e1203d8.
    
    # Conflicts:
    #	pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/services/OwnerService.java
    #	pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/services/PetService.java
    #	pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/services/VetService.java
    #	pom.xml

--- pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/services/PetService.java
+++
@@ -1,8 +0,0 @@
-package jacreator.spring.sfgpetclinic.services;
-
-import jacreator.spring.sfgpetclinic.mdoel.Pet;
-
-import java.util.Set;
-
-public interface PetService extends CrudService<Pet, Long> {
-}
--- pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/services/VetService.java
+++
@@ -1,9 +0,0 @@
-package jacreator.spring.sfgpetclinic.services;
-
-import jacreator.spring.sfgpetclinic.mdoel.Vet;
-
-import java.util.Set;
-
-public interface VetService extends CrudService<Vet, Long> {
-
-}
