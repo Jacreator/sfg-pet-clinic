357a3d2e88048f990cd0021a6bfbdf75cf17dda7
Jacreator <jambone.james82@gmail.com>
2020-08-23 16:25:16 +0100

    added specialty class and related it to vet as Set. closes #21

---
+++ pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/mdoel/Specialty.java
@@ -0,0 +1,13 @@
+package jacreator.spring.sfgpetclinic.mdoel;
+
+public class Specialty extends BaseEntity {
+    private String Description;
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+}
