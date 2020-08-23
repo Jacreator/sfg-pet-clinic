6b0e7b07cb2af5f0fe4c29cea749088cd427c791
Jacreator <jambone.james82@gmail.com>
2020-08-23 16:43:09 +0100

    added petType service,map and Impl. closes #23

--- pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/mdoel/PetType.java
+++ pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/mdoel/PetType.java
@@ -1,6 +1,6 @@
 package jacreator.spring.sfgpetclinic.mdoel;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
---
+++ pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package jacreator.spring.sfgpetclinic.services;
+
+import jacreator.spring.sfgpetclinic.mdoel.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
