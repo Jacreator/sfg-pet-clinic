4939a38e8b81b89821ef1003eacedbafb5a50a60
Jacreator <jambone.james82@gmail.com>
2020-08-29 15:17:01 +0100

    convert owner to JPA entity closes #30

--- pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/mdoel/Pet.java
+++ pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/mdoel/Pet.java
@@ -1,12 +1,24 @@
 package jacreator.spring.sfgpetclinic.mdoel;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "Pets")
 public class Pet extends Person {
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
--- pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/mdoel/PetType.java
+++ pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/mdoel/PetType.java
@@ -1,6 +1,14 @@
 package jacreator.spring.sfgpetclinic.mdoel;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
+
+    @Column(name = "name")
     private String name;
 
     public String getName() {
