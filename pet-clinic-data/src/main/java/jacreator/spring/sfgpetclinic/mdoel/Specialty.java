edae9dd9cc63f6ba2586b4379d12e45750ecbb79
Jacreator <jambone.james82@gmail.com>
2020-08-29 15:30:10 +0100

    convert vet to JPA entity closes #31

--- pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/mdoel/Specialty.java
+++ pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/mdoel/Specialty.java
@@ -1,6 +1,14 @@
 package jacreator.spring.sfgpetclinic.mdoel;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Specialities")
 public class Specialty extends BaseEntity {
+
+    @Column(name = "description")
     private String Description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/mdoel/Vet.java
+++ pet-clinic-data/src/main/java/jacreator/spring/sfgpetclinic/mdoel/Vet.java
@@ -1,10 +1,16 @@
 package jacreator.spring.sfgpetclinic.mdoel;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "Vets")
 public class Vet extends Person {
 
+    @ManyToMany
+    @JoinTable(name = "vets_specialities",  joinColumns = @JoinColumn(name = "vets_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialities_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
